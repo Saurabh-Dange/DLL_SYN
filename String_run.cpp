b591bb38ac8852ef1c347118ae81902107c8ec6f
Saurabh Dange <sdange.dange303@gmail.com>
2017-06-28 13:35:27 +0530

    Update String_run.cpp

--- String_run.cpp
+++ String_run.cpp
@@ -18,30 +18,23 @@ typedef int(__cdecl *MYPRO)(int);
 
 
 DWORD WINAPI pString(__in LPVOID lpParameter)
+//void pString()
 {
 	HINSTANCE hinstLib;
 	MYPROC ProcAdd;
 	BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
 	int flag = 0;
-	//char str[MAX_LEN];
-	char c;
+	//char *s = new char[MAX_LEN];
+	//cout << "here3";
 
-
-
-	char *s = new char[MAX_LEN];
 	
-	//cin.getline(str, MAX_LEN);
-	//ungetc(c, stdin);
-	//fgets(s, MAX_LEN, stdin);
 	
-
-
-
+	
 	// Get a handle to the DLL module.
 
 	hinstLib = LoadLibrary(TEXT("String_runtime.dll"));
 	
-
+	//cout << "here1";
 
 	// If the handle is valid, try to get the function address.
 
@@ -55,24 +48,26 @@ DWORD WINAPI pString(__in LPVOID lpParameter)
 		if (NULL != ProcAdd)
 		{
 			fRunTimeLinkSuccess = TRUE;
-			
-			//while (1)
-			//{	
-	
-			/*	if (turn) 
-				{
-					while (!mutex);
-					mutex = 0; 
-				}*/
-				//fgets(s, MAX_LEN, stdin);
-				(ProcAdd)("Hello Everyone");
-				/*turn = 0;
-				mutex = 1;*/
+		//	fgets(s, MAX_LEN, stdin);
+			//cout << "here4";
 
-				
-			//}
 			
-			
+		    while (!mutex);
+			if (turn&&mutex)
+			{
+
+				mutex = 0;
+				if ((ProcAdd)("Hello Everyone"))
+
+				{
+					turn = 0;
+					mutex = 1;
+				}
+
+				//cout << "here6";
+			}
+
+							
 		}
 		// Free the DLL module.
 		
@@ -89,52 +84,54 @@ DWORD WINAPI pString(__in LPVOID lpParameter)
 }
 
 
-DWORD WINAPI pCube(__in LPVOID lpParameter)
-{
-	HINSTANCE hinstLib1;
-	
-	MYPRO ProcAdd1;
-	BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
-	int flag = 0;
-	//int k;
-	
-	 //Get a handle to the DLL module.
+	DWORD WINAPI pCube(__in LPVOID lpParameter)
+	{
+		HINSTANCE hinstLib1;
 
-	
-	hinstLib1 = LoadLibrary(TEXT("String_mul_run.dll"));
-	
+		MYPRO ProcAdd1;
+		BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
+		int flag = 0;
+		//int k;
 
-	// If the handle is valid, try to get the function address.
+		 //Get a handle to the DLL module.
+		//cout << "here7";
+
+		hinstLib1 = LoadLibrary(TEXT("String_mul_run.dll"));
+
+
+		// If the handle is valid, try to get the function address.
 
 		if (hinstLib1 != NULL)
 		{
-		
-		ProcAdd1 = (MYPRO)GetProcAddress(hinstLib1, "cube");
+			//cout << "here8";
+			ProcAdd1 = (MYPRO)GetProcAddress(hinstLib1, "cube");
 
-		 //If the function address is valid, call the function.
+			//If the function address is valid, call the function.
 
 			if (NULL != ProcAdd1)
 			{
+				//cout << "here9";
 				fRunTimeLinkSuccess = TRUE;
-				
-				//while (1)
-				//{
-					
-				/*	if (!turn)
-					{
-						while (!mutex);
-						mutex = 0;
-					}*/
-				
+
+
+				while (!mutex);
+				if ((turn==0)&&mutex)
+				{
+					//cout << "here10";
 					(ProcAdd1)(5);
-					/*turn = 1;
-					mutex = 1;*/
-				//}
+					turn = 1;
+					mutex = 0;
+					//cout << "here11";
+				}
+
+
+
 			}
-		// Free the DLL module.
+			// Free the DLL module.
 
 			fFreeResult = FreeLibrary(hinstLib1);
-			}
+		}
+	
 
 	// If unable to call the DLL function,print this
 	if (!fRunTimeLinkSuccess)
@@ -144,11 +141,16 @@ DWORD WINAPI pCube(__in LPVOID lpParameter)
 	}
 
 int main(int argc, char* argv[])
-{ 
+{	
+
 	HANDLE myhandle,myhandle1; 
 	DWORD mythreadid,mythreadid1; 
-	myhandle = CreateThread(0, 0, pString, 0, 0, &mythreadid); 
+	myhandle = CreateThread(0, 64, pString, 0, 0, &mythreadid); 
+	//cout << "here2";
 	myhandle1 = CreateThread(0, 0, pCube, 0, 0, &mythreadid1); 
+	//cout << "here5";
 	getchar();
+	//pString();
+	
 	return 0;
 }
